3f690d4ed81343e7664f8a7c09fc2563f640edc5
kashvi25 <68169502+kashvi25@users.noreply.github.com>
2020-07-13 21:48:28 -0700

    Update EdittPost.vue

--- src/components/EdittPost.vue
+++ src/components/EdittPost.vue
@@ -1,8 +1,8 @@
+
 <template>
     <div v-if="post" class="edit-post container">
-        <h2>Edit {{ post.subject }} Post</h2>
-        <form @submit.prevent="EdittPost">
-            <div class="add-post container">
+        <h2>Edit {{ post.author }}'s {{ post.subject }} Post</h2>
+        <form @submit.prevent="EditPost">
                 <div class="field subject">
                     <label for="subject">Blog Post Subject:</label>
                     <input type="text" name="subject" v-model="post.subject">
@@ -13,24 +13,19 @@
                 </div>
                 <div class="field date">
                     <label for="date">Date of Post:</label>
-                    <br/>
-                    <label class="grey-text">EX. November 11, 2011</label>
                     <input type="text" name="date" v-model="post.date" >
                 </div>
                 <div class="field time">
                     <label for="time">Time of Post:</label>
-                    <br/>
-                    <label class="grey-text">EX. 11:00 AM</label>
                     <input type="text" name="time" v-model="post.time">
                 </div>
                 <div class="field message">
                     <label for="message">Blog Post Message:</label>
                     <input type="text" name="message" v-model="post.message">
                 </div>
-            </div>
-            <br/>
+                <br/>
         <div class="field center-align">
-            <button class="v-btn x-large">Repost</button>
+            <button class="btn-large depressed">Repost</button>
         </div>
         <br/>
         <br/>
@@ -40,13 +35,42 @@
 
 <script>
 import db from '@/firebase/init'
+import slugify from 'slugify'
 
 export default {
-    name: 'EdittPost',
+    name: 'EditPost',
      data(){
-        return{
-            post: null
+        return {
+            post: null,
     }
+  },
+  methods: {
+      EditPost(){
+            if (this.post.subject){
+                this.feedback = null
+                //create a slug
+                this.post.slug = slugify(this.post.subject, {
+                    replacement: '-',
+                    remove: /[$*_+~.()'"!\-:@]/g,
+                    lower: true
+                })
+                db.collection('posts').doc(this.post.id).update({
+                    subject: this.post.subject,
+                    author: this.post.author, 
+                    date: this.post.date,
+                    time: this.post.time,
+                    message: this.post.message,
+                    slug: this.post.slug
+                }).then(() => {
+                    this.$router.push({ name: 'Blog' })
+                }).catch(err => {
+                    console.log(err)
+                })
+            } else {
+                this.feedback = 'You must enter a blog subject.'
+            }
+        //console.log(this.post.subject, this.post.author, this.post.date, this.post.time, this.post.message)}
+      }
   },
   created(){
       let ref = db.collection('posts').where('slug', '==', this.$route.params.post_slug)
@@ -58,8 +82,36 @@ export default {
       })
   }
 }
+
 </script>
 
 <style>
-
+.edit-post h2 {
+  text-align: center;
+  font-family: 'Raleway', sans-serif;
+  font-size: 30px;
+  color: rgba(79, 79, 79, 0.86);
+  font-weight: 800;
+}
+.btn-large{
+  text-align: center;
+  background-color: #C4C4C4;
+  font-family: 'Raleway', sans-serif;
+}
+button {
+    background-color: #C4C4C4;
+}
+label {
+    font-size: 16px;
+    font-weight: 300;
+    font-family: 'Raleway', sans-serif;
+    color: rgba(79, 79, 79, 0.86);
+}
+input {
+    font-family: 'Raleway', sans-serif;
+}
+.edit-post{
+    padding: 20px;
+    max-width: 500px;
+}
 </style>
